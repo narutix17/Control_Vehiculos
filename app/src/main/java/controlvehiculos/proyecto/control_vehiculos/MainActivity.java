7a8440e88e211e1474335158e9672d8986501dbc
Melanye Suárez <Melanye Surez>
2017-06-02 22:46:43 -0500

    Initial commit

---
+++ app/src/main/java/controlvehiculos/proyecto/control_vehiculos/MainActivity.java
@@ -0,0 +1,13 @@
+package controlvehiculos.proyecto.control_vehiculos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

542afe7a3b3c143fa4cfad7e8439bc2c5b845370
lkuffo <lkuffo@espol.edu.ec>
2017-07-28 00:36:47 -0500

    Added Dependencies for Charts to work

--- myapp/www/js/controllers/gastos.js
+++ myapp/www/js/controllers/gastos.js
@@ -1,12 +1,15 @@
 angular.module('app.controllers')
 
 .controller('gastosController', function($scope, $cordovaSQLite, $rootScope, $ionicLoading) {
-
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
+  $scope.graph = {};
+  $scope.graph.data = [
+    //Awake
+    [16, 15, 20, 12, 16, 12, 8],
+    //Asleep
+    [8, 9, 4, 12, 8, 12, 14]
   ];
+  $scope.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  $scope.graph.series = ['Awake', 'Asleep'];
+
 
 });

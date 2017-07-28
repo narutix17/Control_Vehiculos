24c814ae6fc86b6e3cf28315d570b50ac1220e8d
lkuffo <lkuffo@espol.edu.ec>
2017-07-28 01:38:16 -0500

    Sprint 4. Task 110, 111, 112: Visualizar Gastos de Servicios Realizados a un Vehiculo Completado

--- myapp/www/js/controllers/gastos.js
+++ myapp/www/js/controllers/gastos.js
@@ -1,15 +1,69 @@
 angular.module('app.controllers')
 
 .controller('gastosController', function($scope, $cordovaSQLite, $rootScope, $ionicLoading) {
-  $scope.graph = {};
-  $scope.graph.data = [
-    //Awake
-    [16, 15, 20, 12, 16, 12, 8],
-    //Asleep
-    [8, 9, 4, 12, 8, 12, 14]
-  ];
-  $scope.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  $scope.graph.series = ['Awake', 'Asleep'];
+
+  $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+  });
+
+
+  var query = "SELECT * FROM mantenimiento JOIN servicio ON mantenimiento.idServicio = servicio.id AND servicio.idVehiculo = ?";
+
+
+
+    console.log(query);
+    $scope.selectedVehicleMantenimientos = [];
+    $scope.vehicleMantenimientos = [];
+    $scope.vehicleInfoMantenimientos = [];
+    console.log($rootScope.chosenVehicle.id);
+
+    $scope.graph = {};
+    $scope.graph.data = [
+      []
+    ];
+
+    $scope.graph.labels = [];
+
+    $cordovaSQLite.execute(db, query, [$rootScope.chosenVehicle.id]).then(function(res){
+      if (res.rows.length > 0){
+        for (var i=0; i<res.rows.length; i++) {
+          $scope.fehcaRealiz = res.rows.item(i).fechaRealizado;
+          $scope.fecha = new Date($scope.fehcaRealiz);
+
+          $scope.selectedVehicleMantenimientos.push({
+            nombre: res.rows.item(i).nombre,
+            id: res.rows.item(i).id_m,
+            idServicio: res.rows.item(i).idServicio,
+            detalle: res.rows.item(i).detalle,
+            precio: res.rows.item(i).precio,
+            fecha: $scope.fecha,
+            fechaRealizado: res.rows.item(i).fechaRealizado.substring(0, 15),
+            fechaEntera: res.rows.item(i).fechaRealizado
+          });
+        }
+      }
+
+      $scope.selectedVehicleMantenimientos.sort(function(a,b){
+        a = new Date(a.fechaEntera);
+        b = new Date(b.fechaEntera);
+        return a - b;
+      });
+
+      for (var i = 0; i < $scope.selectedVehicleMantenimientos.length; i++){
+        var item = $scope.selectedVehicleMantenimientos[i]
+        $scope.graph.data[0].push(parseInt(item.precio));
+        $scope.graph.labels.push(item.fechaRealizado.substring(0, 15));
+      }
+
+      $ionicLoading.hide();
+    });
+
+
+  //$scope.graph.series = ['Awake', 'Asleep'];
 
 
 });

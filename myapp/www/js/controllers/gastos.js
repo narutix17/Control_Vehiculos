f627f3628623b3fac1526a62e9cb022d50ed1030
lkuffo <lkuffo@espol.edu.ec>
2017-07-29 18:35:29 -0500

    Sprint 4. Task 95. Elegir intervalo de tiempo para visualizar gastos

--- myapp/www/js/controllers/gastos.js
+++ myapp/www/js/controllers/gastos.js
@@ -2,10 +2,28 @@ angular.module('app.controllers')
 
 .controller('gastosController', function($scope, $cordovaSQLite, $rootScope, $ionicLoading) {
 
+  var byweek = {};
+  var bymonth = {};
+  function groupweek(value, index, array){
+      d = new Date(value.fechaEntera);
+      d = Math.floor(d.getTime()/(1000*60*60*24*7));
+      byweek[d]=byweek[d]||[];
+      byweek[d].push(value);
+  }
+  function groupmonth(value, index, array)
+  {
+      d = new Date(value.fechaEntera);
+      d = (d.getFullYear()-1970)*12 + d.getMonth();
+      bymonth[d]=bymonth[d]||[];
+      bymonth[d].push(value);
+  }
+
   $scope.actGastos = {};
 
   $scope.graficar = function(){
-
+    bymonth = {};
+    byweek = {};
+    var tipo = $scope.actGastos.intervalo;
     var f1 = $scope.actGastos.fechaini;
     var f2 = $scope.actGastos.fechafin;
 
@@ -42,17 +60,63 @@ angular.module('app.controllers')
         return a - b;
       });
 
-      for (var i = 0; i < $scope.selectedVehicleMantenimientos.length; i++){
+      if (tipo === "Diario"){
+        for (var i = 0; i < $scope.selectedVehicleMantenimientos.length; i++){
+          var item = $scope.selectedVehicleMantenimientos[i];
+          minDate = new Date(f1);
+          maxDate = new Date(f2);
+          if (item.fecha >= minDate && item.fecha <= maxDate){
+            console.log("PUNTO");
+            $scope.graph.data[0].push(parseInt(item.precio));
+            $scope.graph.labels.push(item.fechaRealizado.substring(4, 15));
+          }
+        }
+      } else if (tipo === "Semanal") {
 
-        var item = $scope.selectedVehicleMantenimientos[i];
-        minDate = new Date(f1);
-        maxDate = new Date(f2);
-        if (item.fecha >= minDate && item.fecha <= maxDate){
-          console.log("PUNTO");
-          $scope.graph.data[0].push(parseInt(item.precio));
-          $scope.graph.labels.push(item.fechaRealizado.substring(4, 15));
+        var mantenimientosFiltrados = [];
+        for (var i = 0; i < $scope.selectedVehicleMantenimientos.length; i++){
+          var item = $scope.selectedVehicleMantenimientos[i];
+          minDate = new Date(f1);
+          maxDate = new Date(f2);
+          if (item.fecha >= minDate && item.fecha <= maxDate){
+            mantenimientosFiltrados.push(item);
+          }
+        }
+
+        mantenimientosFiltrados.map(groupweek);
+        for (var week in byweek){
+          var group = byweek[week]
+          var weektotal = 0;
+          for (var i in group){
+            weektotal += parseInt(group[i].precio);
+          }
+          $scope.graph.data[0].push(weektotal);
+          $scope.graph.labels.push(week);
+        }
+
+      } else {
+        var mantenimientosFiltrados = [];
+        for (var i = 0; i < $scope.selectedVehicleMantenimientos.length; i++){
+          var item = $scope.selectedVehicleMantenimientos[i];
+          minDate = new Date(f1);
+          maxDate = new Date(f2);
+          if (item.fecha >= minDate && item.fecha <= maxDate){
+            mantenimientosFiltrados.push(item);
+          }
+        }
+
+        mantenimientosFiltrados.map(groupmonth);
+        for (var month in bymonth){
+          var group = bymonth[month]
+          var monthtotal = 0;
+          for (var i in group){
+            monthtotal += parseInt(group[i].precio);
+          }
+          $scope.graph.data[0].push(monthtotal);
+          $scope.graph.labels.push(month);
         }
       }
+
 
     });
   }

6a7bb62c2d083e47230d47e37eab6143464f4079
lkuffo <lkuffo@espol.edu.ec>
2017-07-29 02:26:06 -0500

    Sprint 4. Task 95. Elegir periodo de tiempo para visualizar gastos

--- myapp/www/js/controllers/gastos.js
+++ myapp/www/js/controllers/gastos.js
@@ -2,6 +2,61 @@ angular.module('app.controllers')
 
 .controller('gastosController', function($scope, $cordovaSQLite, $rootScope, $ionicLoading) {
 
+  $scope.actGastos = {};
+
+  $scope.graficar = function(){
+
+    var f1 = $scope.actGastos.fechaini;
+    var f2 = $scope.actGastos.fechafin;
+
+    $scope.selectedVehicleMantenimientos = [];
+    $scope.graph = {};
+    $scope.graph.data = [
+      []
+    ];
+
+    $scope.graph.labels = [];
+
+    $cordovaSQLite.execute(db, query, [$rootScope.chosenVehicle.id]).then(function(res){
+      if (res.rows.length > 0){
+        for (var i=0; i<res.rows.length; i++) {
+          $scope.fehcaRealiz = res.rows.item(i).fechaRealizado;
+          $scope.fecha = new Date($scope.fehcaRealiz);
+
+          $scope.selectedVehicleMantenimientos.push({
+            nombre: res.rows.item(i).nombre,
+            id: res.rows.item(i).id_m,
+            idServicio: res.rows.item(i).idServicio,
+            detalle: res.rows.item(i).detalle,
+            precio: res.rows.item(i).precio,
+            fecha: $scope.fecha,
+            fechaRealizado: res.rows.item(i).fechaRealizado.substring(0, 15),
+            fechaEntera: res.rows.item(i).fechaRealizado
+          });
+        }
+      }
+
+      $scope.selectedVehicleMantenimientos.sort(function(a,b){
+        a = new Date(a.fechaEntera);
+        b = new Date(b.fechaEntera);
+        return a - b;
+      });
+
+      for (var i = 0; i < $scope.selectedVehicleMantenimientos.length; i++){
+
+        var item = $scope.selectedVehicleMantenimientos[i];
+        minDate = new Date(f1);
+        maxDate = new Date(f2);
+        if (item.fecha >= minDate && item.fecha <= maxDate){
+          console.log("PUNTO");
+          $scope.graph.data[0].push(parseInt(item.precio));
+          $scope.graph.labels.push(item.fechaRealizado.substring(4, 15));
+        }
+      }
+
+    });
+  }
+
   $ionicLoading.show({
       content: 'Loading',
       animation: 'fade-in',
@@ -10,10 +65,7 @@ angular.module('app.controllers')
       showDelay: 0
   });
 
-
   var query = "SELECT * FROM mantenimiento JOIN servicio ON mantenimiento.idServicio = servicio.id AND servicio.idVehiculo = ?";
-
-
 
     console.log(query);
     $scope.selectedVehicleMantenimientos = [];
@@ -56,14 +108,10 @@ angular.module('app.controllers')
       for (var i = 0; i < $scope.selectedVehicleMantenimientos.length; i++){
         var item = $scope.selectedVehicleMantenimientos[i]
         $scope.graph.data[0].push(parseInt(item.precio));
-        $scope.graph.labels.push(item.fechaRealizado.substring(0, 15));
+        $scope.graph.labels.push(item.fechaRealizado.substring(4, 15));
       }
 
       $ionicLoading.hide();
     });
-
-
-  //$scope.graph.series = ['Awake', 'Asleep'];
-
 
 });

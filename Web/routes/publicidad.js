fef6b8323fa971607e42ad3ab54fe9e8437b0ff1
lkuffo <lkuffo@espol.edu.ec>
2017-08-19 02:50:35 -0500

    Sprint #7 - Tarea X: Actualizar Publicidad

--- Web/routes/publicidad.js
+++ Web/routes/publicidad.js
@@ -14,14 +14,33 @@ router.delete('/:id', function(req, res, next) {
     Publicidad.find({id: req.params.id}).remove().exec();
 });
 
+function updatePublicidad(req, res, next){
+    Publicidad.findOne({id : req.body.updateId}, function(err, publicidad){
+        publicidad.nombre = req.body.pubName;
+        publicidad.region = req.body.pubRegion;
+        publicidad.url_publicidad = req.body.pubUrl;
+
+        publicidad.save(function(err){
+            return res.send(err);
+        });
+    }
+
+}
+
 router.post('/', function(req, res, next){
+
+    if (req.body._method === "put"){
+      updatePublicidad(req, res, next);
+      return;
+    }
+
     Publicidad.find(function(err, data){
       var newId = Math.max.apply(Math, data.map(function(elem){return elem.id})) + 1 ;
       var publicidad = new Publicidad({
           id: newId,
-          nombre: req.body["pubName"],
-          region: req.body["pubRegion"],
-          url_publicidad: req.body["pubUrl"],
+          nombre: req.body.pubName,
+          region: req.body.pubRegion,
+          url_publicidad: req.body.pubUrl,
           fechaAgregada: new Date()
       });
 

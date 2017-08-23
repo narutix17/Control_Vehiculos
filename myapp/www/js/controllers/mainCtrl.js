bc80dbf6e8443c8458c7f963353d8ce4bd700a46
lkuffo <lkuffo@espol.edu.ec>
2017-08-23 01:40:20 -0500

    Ubicacion de la publicidad correcta

--- myapp/www/js/controllers/mainCtrl.js
+++ myapp/www/js/controllers/mainCtrl.js
@@ -14,7 +14,7 @@ app.controller("MainCtrl", ['$scope', '$http', '$cordovaSQLite', '$rootScope', '
 
 
 
-	var procesoDePublicidades = setInterval(mostrarPublicidad, 5 * 1000);
+	var procesoDePublicidades = setInterval(mostrarPublicidad, 10 * 1000);
 
 	function mostrarPublicidad(){
 
